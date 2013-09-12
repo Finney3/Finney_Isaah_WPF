3743720add51333bd511948d72b869ac61ece6a0
Finney3 <Finney3@fullsail.edu>
2013-09-12 09:26:08 -0400

    Assignment Operators Plus Example

--- expressions/js/script.js
+++ expressions/js/script.js
@@ -1,10 +1,6 @@
-//Casting
+//Expressions - Assignment Operators
 
-var areaCode = 407;
-var firstPart = 203;
-var secPart = 4529;
-//(407) 203-4529
-
-var phoneNo = "("+ String(areaCode) +")" +String(firstPart) + "-" + String(secPart);
-console.log(phoneNo);
+var a = 3;
+a += 4;
+console.log(a);
 
