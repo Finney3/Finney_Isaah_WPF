8f2a895c9f5c94fdb3c3f768233a76c4d7ed6c36
Finney3 <Finney3@fullsail.edu>
2013-09-12 02:05:22 -0400

    This is Age Example

--- expressions/js/script.js
+++ expressions/js/script.js
@@ -1,6 +1,6 @@
-//Expressions
+//Expressions - Age Example
 
-var a = 2; //Sets up our variable and defines it with the value of 2
-var b;
-b = a + 3; //adds 3 to a
-console.log(a);
+var yearBorn = 1974
+var age= 2012 - yearBorn;
+console.log(age);
+ 
