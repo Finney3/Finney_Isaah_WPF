1b10b6a07589dc69fff71d94b9db0acd0a66c1ca
Finney3 <Finney3@fullsail.edu>
2013-09-18 21:27:00 -0400

    else statement

--- conditional/js/script.js
+++ conditional/js/script.js
@@ -1,13 +1,14 @@
 //Conditional Logic - Relational Expressions
 
-var kidHeight = 49;
+var kidHeight = 47;
 var minHeight = 48;
-var sneakerHeight = 2;
 
 //if the child is old enough, print to the console "you can ride!"
 //if the kid is over 48 inches in height
-if(kidHeight + sneakerHeight > minHeight){
+if(kidHeight > minHeight){
 	//code performed if condition is true
-	console.log("You can ride the coaster!")
-}
+	console.log("You can ride the coaster!");
+}else(kidHeight < minHeight){
+	console.log("Sorry kid, you 've got some growing to do first!");
 
+}
