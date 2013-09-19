7077aabab6659d4f8b0c79ba293a4dd374e71ab4
Finney3 <Finney3@fullsail.edu>
2013-09-19 18:01:18 -0400

    Logical

--- conditional/js/script.js
+++ conditional/js/script.js
@@ -1,14 +1,13 @@
-//Conditional Logic - Relational Expressions
+//Conditional Logic - Logical Operators
 
-var kidHeight = 47;
-var minHeight = 48;
+var budget = 100;
+var iPhonePrice = 199.99;
+var wonLottery = true;
 
-//if the child is old enough, print to the console "you can ride!"
-//if the kid is over 48 inches in height
-if(kidHeight > minHeight){
-	//code performed if condition is true
-	console.log("You can ride the coaster!");
-}else(kidHeight < minHeight){
-	console.log("Sorry kid, you 've got some growing to do first!");
+//if the price of the phone is less our budget AND if out paycheck is over 300
+if(iPhonePrice < budge || wonLottery === true){
+	console.log("We can buy the phone!");
+}else{
 
+	console.log("No phone for you!");
 }
