2ecbaf0c4b0124f9debb5e6a2966c55d60497785
Finney3 <Finney3@fullsail.edu>
2013-11-13 08:08:00 -0500

    Added a military discount

--- expressions/Industry/js/script.js
+++ expressions/Industry/js/script.js
@@ -1,8 +1,10 @@
 // Expressions Industry Isaah Finney 
 
+alert("Military Discount of 20%");
 var newCar = [ "Ford Mustang", "Dodge Charger", "Chrysler 300", "Chevy Camaro"];
 console.log(newCar);
 var type = prompt("type 0 for Ford Mustang, type 1 for Dodge Charger, type 2 for Chrysler 300, type 3 for Chevy Camaro");
 console.log(type);
-var prices = [35000,  40000,  27000, 33000];
+var prices = [35000,  40000,  27000, 33000];//Prices in thousands
 console.log(prices);
+
