987ace8644e468aefe14dd1aabfcb0c5a4db3f89
Finney3 <Finney3@fullsail.edu>
2013-11-13 06:28:24 -0500

    Car prices in array

--- expressions/Industry/js/script.js
+++ expressions/Industry/js/script.js
@@ -4,3 +4,4 @@ var newCar = [ "Ford Mustang", "Dodge Charger", "Chrysler 300", "Chevy Camaro"];
 console.log(newCar);
 var type = prompt("type 1 for Ford Mustang, type 2 for Dodge Charger, type 3 for Chrysler 300, type 4 for Chevy Camaro");
 console.log(type);
+var prices = [35,000 40,000 27,000 33,000];
