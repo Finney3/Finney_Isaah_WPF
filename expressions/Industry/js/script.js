c18f9f2805b5bc69a92d6c8751df5a9df157e383
Finney3 <Finney3@fullsail.edu>
2013-11-13 06:35:47 -0500

    Changed prompt so it read 0-3 for the array index

--- expressions/Industry/js/script.js
+++ expressions/Industry/js/script.js
@@ -2,7 +2,7 @@
 
 var newCar = [ "Ford Mustang", "Dodge Charger", "Chrysler 300", "Chevy Camaro"];
 console.log(newCar);
-var type = prompt("type 1 for Ford Mustang, type 2 for Dodge Charger, type 3 for Chrysler 300, type 4 for Chevy Camaro");
+var type = prompt("type 0 for Ford Mustang, type 1 for Dodge Charger, type 2 for Chrysler 300, type 3 for Chevy Camaro");
 console.log(type);
 var prices = [35000,  40000,  27000, 33000];
 console.log(prices);
