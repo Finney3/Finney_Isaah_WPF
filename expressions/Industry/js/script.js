ea52db39f9db0be329a4317a715f64e97d81566e
Finney3 <Finney3@fullsail.edu>
2013-11-13 06:34:48 -0500

    Code without commas in the array

--- expressions/Industry/js/script.js
+++ expressions/Industry/js/script.js
@@ -4,5 +4,5 @@ var newCar = [ "Ford Mustang", "Dodge Charger", "Chrysler 300", "Chevy Camaro"];
 console.log(newCar);
 var type = prompt("type 1 for Ford Mustang, type 2 for Dodge Charger, type 3 for Chrysler 300, type 4 for Chevy Camaro");
 console.log(type);
-var prices = [35/,000,  40/,000,  27/,000,  33/,000];
+var prices = [35000,  40000,  27000, 33000];
 console.log(prices);
