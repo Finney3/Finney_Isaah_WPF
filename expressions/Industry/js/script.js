d1e9c735590389f3e51713755cfea47a74114b2f
Finney3 <Finney3@fullsail.edu>
2013-11-13 06:26:59 -0500

    Console.log for what users input was

--- expressions/Industry/js/script.js
+++ expressions/Industry/js/script.js
@@ -3,3 +3,4 @@
 var newCar = [ "Ford Mustang", "Dodge Charger", "Chrysler 300", "Chevy Camaro"];
 console.log(newCar);
 var type = prompt("type 1 for Ford Mustang, type 2 for Dodge Charger, type 3 for Chrysler 300, type 4 for Chevy Camaro");
+console.log(type);
