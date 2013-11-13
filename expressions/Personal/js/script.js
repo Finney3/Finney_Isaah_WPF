9ac425451834ddbe1e5af97d053d56ac9b241b42
Finney3 <Finney3@fullsail.edu>
2013-11-12 20:40:35 -0500

    Combined Variables

--- expressions/Personal/js/script.js
+++ expressions/Personal/js/script.js
@@ -3,5 +3,5 @@ alert("M4/M9 Combat Ammo Calculator");
 alert ("It's safe to load a 30 round magazine with only 29 rounds so your weapon does not double feed");
 var m4ammo = prompt("How many M4 magazines do you have?");
 var m4total = m4ammo * 29;
-console.log(m4total);
-//alert("Total rounds needed base of your number of magazines is" m4total);
+var total =  'is the total amount of ammo needed to fill';
+console.log(m4total,total, m4ammo magazines);
