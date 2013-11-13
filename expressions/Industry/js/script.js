65e9fa989a970b411c45eec682b31d5187f35a0b
Finney3 <Finney3@fullsail.edu>
2013-11-13 06:24:59 -0500

    Promp for what car price will be given

--- expressions/Industry/js/script.js
+++ expressions/Industry/js/script.js
@@ -2,3 +2,4 @@
 
 var newCar = [ "Ford Mustang", "Dodge Charger", "Chrysler 300", "Chevy Camaro"];
 console.log(newCar);
+prompt("type 1 for Ford Mustang, type 2 for Dodge Charger, type 3 for Chrysler 300, type 4 for Chevy Camaro");
