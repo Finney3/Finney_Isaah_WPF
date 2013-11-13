46a13c30f8a980525fd259e380bcf9da5e423ad5
Finney3 <Finney3@fullsail.edu>
2013-11-12 20:24:25 -0500

    Personal Expressions js

---
+++ expressions/Personal/js/script.js
@@ -0,0 +1,7 @@
+/* Isaah Finney Personal Expression*/
+alert("M4/M9 Combat Ammo Calculator");
+alert ("It's safe to load a 30 round magazine with only 29 rounds so your weapon does not double feed");
+var m4ammo = prompt("How many M4 magazines do you have?");
+var m4total = m4ammo * 29;
+console.log(m4total);
+//alert("Total rounds needed base of your number of magazines is" m4total);
