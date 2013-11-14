4896cbd54d7a10e5d83222c26dc1c140dcfa335d
Finney3 <Finney3@fullsail.edu>
2013-11-13 23:15:05 -0500

    Working to get Conditionals to work

--- expressions/Industry/js/script.js
+++ expressions/Industry/js/script.js
@@ -3,26 +3,24 @@
 alert("Military Discount of 20%");
 var newCar = [ "Ford Mustang", "Dodge Charger", "Chrysler 300", "Chevy Camaro"];
 console.log(newCar);
+
 var type = prompt("type 0 for Ford Mustang, type 1 for Dodge Charger, type 2 for Chrysler 300, type 3 for Chevy Camaro");
 console.log(type);
+
 var prices = [35000,  40000,  27000, 33000];//Prices in thousands
 console.log(prices);
 
-if (type[0] === 0)
-{
-	var a = console.log(ford * 3500/2);
-}
-else if (type[1] === 1)
-{
+if (type[0] === 0){
+	var a =(3500/2);
+	console.log(a);
+}else if (type[1] === 1){
 	var b = console.log(4000/20);
 }
-else (type[2] === 2)
-{
+else (type[2] === 2){
 	var c = console.log(27000/20);
 }
-else (type[3] === 3)
-{
+else (type[3] === 3){
 	var d = console.log(33000/20);
 }
 //var ford = newCar[0]
-//console.log(ford*3500/20);
+//console.log(ford*3500/20);*/
