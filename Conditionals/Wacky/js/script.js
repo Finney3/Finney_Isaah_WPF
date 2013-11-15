e09ba375e1da56e783b88f9c315dbc83e8cc615b
Finney3 <Finney3@fullsail.edu>
2013-11-14 20:40:23 -0500

    All the teams into a promt for user input

--- Conditionals/Wacky/js/script.js
+++ Conditionals/Wacky/js/script.js
@@ -3,10 +3,10 @@
 //This project is to determine the champioship winners of a 4 team tournament bracket
 
 //These are the var for the 4 teams 1st match ups
-var team1 = prompt("Profuse Gaming Final Score?);
-var team2 = 13
-var team3 = 4
-var team4 = 16
+var team1 = prompt("Profuse Gaming Final Score?");   // User input for var storage
+var team2 = prompt ("Back2Back Gaming Final Score");
+var team3 = prompt ("High Rollers Gaming");
+var team4 = prompt ("Evil Macs");
 
 //This is where the first 2 teams will play and we will have a winner and a loser
 
