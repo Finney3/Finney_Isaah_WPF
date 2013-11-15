6c107f317277fd4e6bfa79c61ec9b314fcf3a47f
Finney3 <Finney3@fullsail.edu>
2013-11-14 23:10:22 -0500

    Teams Conditionals

--- Conditionals/Wacky/js/script.js
+++ Conditionals/Wacky/js/script.js
@@ -3,13 +3,25 @@
 //This project is to determine the champioship winners of a 4 team tournament bracket
 
 //These are the var for the 4 teams 1st match ups
-var team1 = prompt("Profuse Gaming Final Score?");   // User input for var storage
-var team2 = prompt ("Back2Back Gaming Final Score");
-var team3 = prompt ("High Rollers Gaming");
-var team4 = prompt ("Evil Macs");
+var team1 = prompt("Profuse Gaming Final Score?")   // User input for var storage
+var team2 = prompt ("Back2Back Gaming Final Score")
+var team3 = prompt ("High Rollers Gaming Final Score")
+var team4 = prompt ("Evil Macs Final Score")
 
-//This is where the first 2 teams will play and we will have a winner and a loser
+//Display of all the team score user inputs
+console.log(team1);
+console.log(team2);
+console.log(team3);
+console.log(team4);
 
 
-
+//To determine what team will advance to the play the next team
+if (team1 > team2){
+	console.log("Profuse Gaming");
+}else
+	console.log("Back2Back Gaming");
+if (team3 < team4){
+	console.log("High Rollers Gaming");
+}else
+		console.log("Evil Macs");
 
