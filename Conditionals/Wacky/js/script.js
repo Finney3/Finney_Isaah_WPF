cf721b3061ca00551791472fb4fb45afc08e42c7
Finney3 <Finney3@fullsail.edu>
2013-11-14 20:32:30 -0500

    Start of my js

---
+++ Conditionals/Wacky/js/script.js
@@ -0,0 +1,12 @@
+// Conditionals Wacky Isaah Finney 
+
+//I'm buying a pair of shoes the size of 9 but decide to buy another pair if they don't fit my feet
+
+var shoeSize = 9;
+var largeFoot = 10
+var diffSize;
+
+if (shoeSize <= largeFoot){
+	console.log("You have the right size to buy");
+}else
+	console.log("Try getting a larger pair of shoes");
