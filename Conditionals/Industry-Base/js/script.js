3c20c44ba7a63da8b26effa36a4c816a8d4af5a8
Finney3 <Finney3@fullsail.edu>
2013-11-16 04:07:32 -0500

    variables set up for time checks base on users computer for website links

--- Conditionals/Industry-Base/js/script.js
+++ Conditionals/Industry-Base/js/script.js
@@ -2,5 +2,9 @@
 
 //This code will deplict on the time on your computer to determine a website to go to as far as being based on your time
 
+// Variables for the timing depending what website will open
+var timeEarly = "00:00:00";
+var timeMid = "12:00:00";
+var timeLate = "18:00:00";
 
 
