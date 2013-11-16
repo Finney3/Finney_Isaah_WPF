65ff8bbf10d6494cbcb7c28f4e9e0a521cbc8681
Finney3 <Finney3@fullsail.edu>
2013-11-16 04:20:01 -0500

    Pre before going to using the methods to make this code run easier and not be so confusing by using to much random code.

--- Conditionals/Industry-Base/js/script.js
+++ Conditionals/Industry-Base/js/script.js
@@ -7,6 +7,10 @@ var timeEarly = "00:00:00";
 var timeMid = "11:59:00";
 var timeLate = "18:00:00";
 
+//Second test with the easier way to run this code using the get hours and take down some of the vars in this assignment to keep the code small and more simple
+
+
 if (timeEarly => timeMid && timeEarly =< timeLate);}
 	console.log("www.fragfest-gaming.com");
-{
+{else
+	console.log("https://www.facebook.com/pages/Somerset-Web-Solutions/575532319140161?ref=hl");
