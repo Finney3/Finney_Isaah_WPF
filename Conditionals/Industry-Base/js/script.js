ee4e47eb642f033ed908fe37858ff9c640b48e19
Finney3 <Finney3@fullsail.edu>
2013-11-16 04:56:45 -0500

    Tenary code will now come into play

--- Conditionals/Industry-Base/js/script.js
+++ Conditionals/Industry-Base/js/script.js
@@ -3,14 +3,24 @@
 //This code will deplict on the time on your computer to determine a website to go to as far as being based on your time
 
 // Variables for the timing depending what website will open
-var timeEarly = "00:00:00";
+/*var timeEarly = "00:00:00";
 var timeMid = "11:59:00";
-var timeLate = "18:00:00";
+var timeLate = "18:00:00";*/
 
-//Second test with the easier way to run this code using the get hours and take down some of the vars in this assignment to keep the code small and more simple
+/*Second test with the easier way to run this code using the get hours and take down some of the vars in this assignment to keep the code small and more simple
+I used  the date method just to make this easier because trying to create all tyes of varibles and without using the date method this idea would never work unless just a user input there own time that
+is on there computer but then I would also have to make sure the formatt is right in order to compare. Makes more sense to use the tools that are there and save time on the code.
+*/
+var t= new Date();
+var cHour = t.getHours();
+var cMin = t.getMinutes();
+var cSec= t.getSeconds();
 
+if((cHour > 12) || (cHour == 12 && cMin == 0 && cSec == 0 )){
+   console.log("Fragfest-gaming.com");
+} else {
+   console.log("https://www.facebook.com/pages/Somerset-Web-Solutions/575532319140161?ref=hl");
+}
 
-if (timeEarly => timeMid && timeEarly =< timeLate);}
-	console.log("www.fragfest-gaming.com");
-{else
-	console.log("https://www.facebook.com/pages/Somerset-Web-Solutions/575532319140161?ref=hl");
+//This is where I will make turn this code into tenary
+
