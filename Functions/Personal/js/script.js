787d9b4948708bc9e17449b01e4d84dc7d795636
Finney3 <Finney3@fullsail.edu>
2013-11-26 18:53:58 -0500

    Setting up the If Statement

--- Functions/Personal/js/script.js
+++ Functions/Personal/js/script.js
@@ -1,15 +1,18 @@
 // Functions Personal Isaah Finney November 21, 2013
 
 //Call For Fire Function
-callForfire(4658)
-function callForFire(){
+callForfire(4658)//This is the 4 Digit Grid I'm working Off
+function callForfire(){
 	var friendlies = 3368; 
 	var enimeies =4658;
 	var dangerClose="It is not safe to fire ammunitions";
 	var repeatFire="We have been cleared hot";
-if(){
+if(friendlies < !OR > enimeies){
+	console.log(repeatFire);
+} else
 
-}	
+		console.log(dangerClose);
 
 }
+callForfire();
 
