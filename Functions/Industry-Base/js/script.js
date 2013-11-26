4e3cc564cb2afa2afa45ca7799a3a895292987cc
Finney3 <Finney3@fullsail.edu>
2013-11-25 20:00:52 -0500

    Prompting for Age In Function

--- Functions/Industry-Base/js/script.js
+++ Functions/Industry-Base/js/script.js
@@ -1,3 +1,11 @@
 // Function Industry Isaah Finney November 21, 2013
 
+//Function Checks The Age, Allows to Enter a Website
+
+function ageFunction(){
+	var a = prompt("How old are you?");
+}
+
+ageFunction();
+
 
