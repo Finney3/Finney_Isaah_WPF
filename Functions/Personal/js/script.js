d9f807df94f9ef6b69d334861b9b7fa16ada97d0
Finney3 <Finney3@fullsail.edu>
2013-11-26 17:31:22 -0500

    Variables set up

--- Functions/Personal/js/script.js
+++ Functions/Personal/js/script.js
@@ -1,2 +1,15 @@
 // Functions Personal Isaah Finney November 21, 2013
 
+//Call For Fire Function
+callForfire(4658)
+function callForFire(){
+	var friendlies = 3368; 
+	var enimeies =4658;
+	var dangerClose="It is not safe to fire ammunitions";
+	var repeatFire="We have been cleared hot";
+if(){
+
+}	
+
+}
+
