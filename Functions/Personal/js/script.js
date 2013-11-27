491b670aeb106ee678c3078a689d6969fdce2924
Finney3 <Finney3@fullsail.edu>
2013-11-27 15:52:16 -0500

    Change the the Variables to a global variable.

--- Functions/Personal/js/script.js
+++ Functions/Personal/js/script.js
@@ -1,16 +1,21 @@
 // Functions Personal Isaah Finney November 21, 2013
 
-//Call For Fire Function
-function callForfire(){
 	var friendlies = 4985; // Declares friendlies value
-	var enimeies =4658; // Declares enimeies value
+	var enimies =4658; // Declares enimeies value
 	var dangerClose="It is not safe to fire ammunitions"; // string to print to console if condition is true
 	var repeatFire="We have been cleared hot"; //string to print to console if the condition is false
-if(friendlies > enimeies){
-	 return console.log(repeatFire);
+
+//Call For Fire Function
+function callForfire(){
+	friendlies = 4985;
+	enimies = 4658;
+	dangerClose = "U=It is not safe to fire ammunition";
+	repeatFire = "We have been cleared hot";
+if(friendlies > enimies){
+console.log(repeatFire);
 } else
 
-		return console.log(dangerClose);
+console.log(dangerClose);
 
 }
 callForfire();
