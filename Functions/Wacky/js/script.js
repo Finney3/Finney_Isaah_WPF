2e1b490934c0edb2c7975d937fb3cfc9ee214a52
Finney3 <Finney3@fullsail.edu>
2013-11-27 16:26:35 -0500

    Parameters into function to be called without vars

--- Functions/Wacky/js/script.js
+++ Functions/Wacky/js/script.js
@@ -1,3 +1,10 @@
 // Functions Wacky Isaah Finney November 21, 2013
 
+//This function is to help calculate the frag per round based on number from a player in a online game
+
+function calcFpr (16,14,"Peekay"){
+
+
+}
+
 
